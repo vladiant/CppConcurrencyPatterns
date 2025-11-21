ea36143933f122a1071a2d70c031b26cce70e516
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-11-21 08:00:22 +0200

    Add OMP Critical Section example

---
+++ OpenMP/CriticalSection/main.cpp
@@ -0,0 +1,20 @@
+#include <omp.h>
+
+#include <cstdio>
+#include <thread>
+
+int main() {
+  printf("Start\n");
+
+  unsigned int n = std::thread::hardware_concurrency();
+  printf("%d concurrent threads are supported.\n", n);
+
+#pragma omp parallel
+  {
+#pragma omp critical
+    printf("Thread %d\n", omp_get_thread_num());
+  }
+
+  printf("End\n");
+  return 0;
+}

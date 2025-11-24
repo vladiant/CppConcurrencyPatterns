0fc61fe3ed3939b7af46f81dc6234aca708f457e
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-11-24 20:42:50 +0200

    Add OMP reductions sample

---
+++ OpenMP/Reductions/main.cpp
@@ -0,0 +1,29 @@
+#include <omp.h>
+
+#include <cstdio>
+
+int main() {
+  printf("Start\n");
+
+  size_t n = 12;
+  int a[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  int sum = 0;
+
+#pragma omp parallel shared(n, a, sum) default(none)
+  {
+// Should be only for loop
+// Number of iterations should be known before loop start
+// No early exits
+// Only random access iterators
+#pragma omp for
+    for (size_t i = 0; i < n; i++) {
+#pragma omp atomic
+      sum += a[i];
+    }
+  }
+
+  printf("sum: %d\n", sum);
+
+  printf("End\n");
+  return 0;
+}
